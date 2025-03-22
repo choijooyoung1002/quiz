ead50f8e7389d23b93ac90c2678f938c2d509d51
CHOI JU YOUNG <82010748+choijooyoung1002@users.noreply.github.com>
2025-03-22 22:57:56 +0900

    Create test.js

---
+++ test.js
@@ -0,0 +1,7 @@
+// @name Alert test
+// @description Displays an alert dialog on google.it
+// @author Emanuele Faranda
+// @version 1.0
+// @match *://book.ebs.co.kr/*
+
+alert(window.navigator.userAgent);
